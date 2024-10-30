4bb6118cfd8919873d276ef2da24a06fd5feb129
Iesu16 <136399963+Iesu16@users.noreply.github.com>
2024-10-29 19:09:52 -0500

    "TALLER_9 - Paso 1: Implementación de subconsultas complejas"

--- TALLERES/TALLER_9/subconsultas_pdo.php
+++ TALLERES/TALLER_9/subconsultas_pdo.php
@@ -1,6 +1,5 @@
 <?php
 require_once "config_pdo.php";
-
 try {
     // 1. Productos que tienen un precio mayor al promedio de su categoría
     $sql = "SELECT p.nombre, p.precio, c.nombre as categoria,
